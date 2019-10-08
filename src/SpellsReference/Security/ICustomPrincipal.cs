ca3b82a391044877cf49da82a3ba44ee6cf62141
jbvera <johnbenedictv@gmail.com>
2019-10-08 15:19:32 -0400

    Add CustomIdentity and CustomPrincipal

---
+++ src/SpellsReference/Security/ICustomPrincipal.cs
@@ -0,0 +1,10 @@
+﻿namespace SpellsReference.Security
+{
+    public class ICustomPrincipal
+    {
+        int Id { get; }
+        string Username { get; }
+        string FirstName { get; }
+        string LastName { get; }
+    }
+}
