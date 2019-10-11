141f528a7a05629f98871c8657fba213425b6e17
wjlawlor <wjl28@msn.com>
2019-10-11 16:12:46 -0400

    React skeleton with routing.

---
+++ src/SpellsReference/Components/App.js
@@ -0,0 +1,44 @@
+﻿import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Spells from './Spells';
+
+function About() {
+    return (
+        <h3>About</h3>
+    );
+}
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Spells</Link>
+                        </li>
+                        <li>
+                            <Link to="/About">About</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/About">
+                        <About />
+                    </Route>
+                    <Route path="/">
+                        <Spells />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/SpellsReference/Components/Spells.js
@@ -0,0 +1,74 @@
+﻿import React, { Component } from 'react';
+
+function Spell(props) {
+    return (
+        <tr>
+            <td>{props.spell.name}</td>
+            <td>{props.spell.level}</td>
+            <td>{props.spell.school}</td>
+            <td>{props.spell.castingTime}</td>
+            <td>{props.spell.range}</td>
+            <td>{props.spell.verbal ? 'True' : 'False'}</td>
+            <td>{props.spell.somatic ? 'True' : 'False'}</td>
+            <td>{props.spell.materials}</td>
+            <td>{props.spell.duration}</td>
+            <td>{props.spell.ritual ? 'True' : 'False'}</td>
+            <td>{props.spell.description}</td>
+        </tr>
+    );
+}
+
+class Spells extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            spells: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:61211/api/spell')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    spells: data.spells
+                });
+            });
+    }
+
+    render() {
+        return (
+            <div className="mt-2">
+                <h1>Spell Index Page</h1>
+                <div>
+                    <table className="table table-sm table-hover">
+                        <thead className="thead thead-dark">
+                            <tr>
+                                <th>Name</th>
+                                <th>Level</th>
+                                <th>School</th>
+                                <th>Cast Time</th>
+                                <th>Range</th>
+                                <th>Verbal</th>
+                                <th>Somatic</th>
+                                <th>Materials</th>
+                                <th>Duration</th>
+                                <th>Ritual</th>
+                                <th>Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.spells.map(spell => (
+                                <Spell spell={spell} key={spell.id} />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Spells;
