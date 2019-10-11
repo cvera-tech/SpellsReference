788e3c8bbbce0fc3c4bb147f226847159497d51f
wjlawlor <wjl28@msn.com>
2019-10-11 15:23:32 -0400

    React initial page imitation set up.

--- src/SpellsReference/Components/index.js
+++ src/SpellsReference/Components/index.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom';
 
 function Spell(props) {
     return (
-      <tr>
-        <td>{props.spell.name}</td>
-        <td>{props.spell.level}</td>
-        <td>{props.spell.school}</td>
-        <td>{props.spell.castingTime}</td>
-        <td>{props.spell.range}</td>
-        <td>{props.spell.verbal.toString()}</td>
-        <td>{props.spell.somatic.toString()}</td>
-        <td>{props.spell.materials}</td>
-        <td>{props.spell.duration}</td>
-        <td>{props.spell.ritual.toString()}</td>
-        <td>{props.spell.description}</td>
-      </tr>
+        <tr>
+            <td>{props.spell.name}</td>
+            <td>{props.spell.level}</td>
+            <td>{props.spell.school}</td>
+            <td>{props.spell.castingTime}</td>
+            <td>{props.spell.range}</td>
+            <td>{props.spell.verbal.toString().charAt(0).toUpperCase() + props.spell.verbal.toString().slice(1)}</td>
+            <td>{props.spell.somatic.toString().charAt(0).toUpperCase() + props.spell.somatic.toString().slice(1)}</td>
+            <td>{props.spell.materials}</td>
+            <td>{props.spell.duration}</td>
+            <td>{props.spell.ritual.toString().charAt(0).toUpperCase() + props.spell.ritual.toString().slice(1)}</td>
+            <td>{props.spell.description}</td>
+        </tr>
     );
 }
 
@@ -33,7 +33,6 @@ class App extends Component {
         fetch('http://localhost:61211/api/spell')
             .then(response => response.json())
             .then(data => {
-                console.log(data)
                 this.setState({
                     spells: data.spells
                 });
@@ -42,35 +41,35 @@ class App extends Component {
 
     render() {
         return (
-          <div>
-            <h3>Display Spells</h3>
-            <div>
-              <table htmlclass="table table-sm">
-                <thead htmlclass="thead thead-dark">
-                  <tr>
-                    <th>Name</th>
-                    <th>Level</th>
-                    <th>School</th>
-                    <th>Cast Time</th>
-                    <th>Range</th>
-                    <th>Verbal</th>
-                    <th>Somatic</th>
-                    <th>Materials</th>
-                    <th>Duration</th>
-                    <th>Ritual</th>
-                    <th>Description</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.state.spells.map(spell => (
-                      <Spell spell={spell} key={spell.id} />
-                  ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    );
-            }
+            <div className="mt-2">
+                <h1>Spell Index Page</h1>
+                <div>
+                    <table className="table table-sm table-hover">
+                        <thead className="thead thead-dark">
+                            <tr>
+                                <th>Name</th>
+                                <th>Level</th>
+                                <th>School</th>
+                                <th>Cast Time</th>
+                                <th>Range</th>
+                                <th>Verbal</th>
+                                <th>Somatic</th>
+                                <th>Materials</th>
+                                <th>Duration</th>
+                                <th>Ritual</th>
+                                <th>Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.spells.map(spell => (
+                                <Spell spell={spell} key={spell.id} />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
