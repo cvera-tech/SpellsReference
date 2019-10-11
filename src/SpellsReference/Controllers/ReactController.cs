21f247dc3ebf7980af4b9c9a80e5aa2e836cda18
jbvera <johnbenedictv@gmail.com>
2019-10-11 11:51:42 -0400

    Add React

---
+++ src/SpellsReference/Controllers/ReactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpellsReference.Controllers
+{
+    public class ReactController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
