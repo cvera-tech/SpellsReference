0cb12426de3b9d9ce453c2413cff42203e5df4e0
wjlawlor <wjl28@msn.com>
2019-10-11 15:07:44 -0400

    Working fetch.

--- src/SpellsReference/Components/index.js
+++ src/SpellsReference/Components/index.js
@@ -1,15 +1,23 @@
 ﻿import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-//function Spell(props) {
-//    return (
-//      <tr>
-//        <td>{props.spell.Name}</td>
-//        <td>{props.spell.Level}</td>
-//        <td>{props.spell.School}</td>
-//      </tr>
-//    );
-//}
+function Spell(props) {
+    return (
+      <tr>
+        <td>{props.spell.name}</td>
+        <td>{props.spell.level}</td>
+        <td>{props.spell.school}</td>
+        <td>{props.spell.castingTime}</td>
+        <td>{props.spell.range}</td>
+        <td>{props.spell.verbal.toString()}</td>
+        <td>{props.spell.somatic.toString()}</td>
+        <td>{props.spell.materials}</td>
+        <td>{props.spell.duration}</td>
+        <td>{props.spell.ritual.toString()}</td>
+        <td>{props.spell.description}</td>
+      </tr>
+    );
+}
 
 class App extends Component {
 
@@ -17,41 +25,45 @@ class App extends Component {
         super(props);
 
         this.state = {
-            spells: [{name: 'mySpell', id: 1}]
+            spells: []
         };
     }
 
-    //componentDidMount() {
-    //    const obj = getList();
-    //    this.setState({
-    //        spells: obj
-    //    });
-    //    debugger;
-    //}
-
-    //async getList() {
-    //    const response = await fetch('http://localhost:61211/api/spell');
-    //    const obj = await response.json();
-    //    return obj;
-    //}
+    componentDidMount() {
+        fetch('http://localhost:61211/api/spell')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                this.setState({
+                    spells: data.spells
+                });
+            });
+    }
 
     render() {
         return (
           <div>
-            <h3>Display Benches</h3>
+            <h3>Display Spells</h3>
             <div>
-              <h1>Benches</h1>
-              <table>
-                <thead>
+              <table htmlclass="table table-sm">
+                <thead htmlclass="thead thead-dark">
                   <tr>
                     <th>Name</th>
+                    <th>Level</th>
+                    <th>School</th>
+                    <th>Cast Time</th>
+                    <th>Range</th>
+                    <th>Verbal</th>
+                    <th>Somatic</th>
+                    <th>Materials</th>
+                    <th>Duration</th>
+                    <th>Ritual</th>
                     <th>Description</th>
-                    <th>Seats</th>
                   </tr>
                 </thead>
                 <tbody>
                   {this.state.spells.map(spell => (
-                    <tr key = {spell.id}><td>{spell.name}</td></tr>
+                      <Spell spell={spell} key={spell.id} />
                   ))}
             </tbody>
           </table>
