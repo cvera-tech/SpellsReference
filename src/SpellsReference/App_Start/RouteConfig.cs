e4bcbf0cda88e9929793a4f5b495be09da0500cd
jbvera <johnbenedictv@gmail.com>
2019-10-15 09:55:18 -0400

    Add MVC route prefix

--- src/SpellsReference/App_Start/RouteConfig.cs
+++ src/SpellsReference/App_Start/RouteConfig.cs
@@ -14,9 +14,15 @@ namespace SpellsReference
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
+                name: "MVC",
+                url: "mvc/{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "",
+                defaults: new { controller = "React", action = "Index" }
             );
         }
     }
