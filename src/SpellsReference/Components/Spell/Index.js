f0f2b991e40b27969c9b045c52f1dd69a0009c84
wjlawlor <wjl28@msn.com>
2019-10-16 17:05:38 -0400

    Sorting!

--- src/SpellsReference/Components/Spell/Index.js
+++ src/SpellsReference/Components/Spell/Index.js
@@ -20,7 +20,6 @@ function Spell(props) {
             <td>{props.spell.materials}</td>
             <td>{props.spell.duration}</td>
             <td>{props.spell.ritual ? 'True' : 'False'}</td>
-            <td>{props.spell.description}</td>
         </tr>
     );
 }
@@ -32,6 +31,7 @@ class Index extends Component {
 
         this.state = {
             spells: [],
+            previousSort: null,
             spellSelected: false,
             spellRedirect: null
         };
@@ -59,30 +59,107 @@ class Index extends Component {
             });
         });
 
+        const getColumnText = (text) => {
+            const containsUpArrow = text.includes('▲');
+            const containsDownArrow = text.includes('▼');
+            if (containsUpArrow || containsDownArrow) {
+                return text.substring(0, text.length-2)
+            } else {
+                return text;
+            }
+        };
+
         document.getElementById('thead').addEventListener('click', (event) => {
             const th = event.target;
-            const sortCol = th.innerHTML;
-            debugger;
-            if (sortCol === "Name") {
+            const sortByColumn = th.getAttribute('data-columnname');
+            const columnType = th.getAttribute('data-columntype');
 
+            const columnText = getColumnText(th.innerText);
+
+            if (this.state.previousSort === sortByColumn && !(this.state.previousSortState === null)) {
+                if (this.state.previousSortState === 'descending') {
+                    th.innerText = columnText + ' ▲';
+                    this.setState((state) => {
+                        return {
+                            spells: this.mySort(state.spells, sortByColumn, columnType, 'ascending'),
+                            previousSortState: "ascending",
+                            previousSort: sortByColumn
+                        };
+                    });
+                }
+                else {
+                    th.innerText = columnText;
+                    this.setState((state) => {
+                        return {
+                            spells: this.mySort(state.spells, sortByColumn, columnType, 'none'),
+                            previousSortState: null,
+                            previousSort: sortByColumn
+                        };
+                    });
+                }
+            }
+            else {
+                document.getElementById('name').innerText = 'Name';
+                document.getElementById('level').innerText = 'Level';
+                document.getElementById('school').innerText = 'School';
+                document.getElementById('castingTime').innerText = 'Cast Time';
+                document.getElementById('range').innerText = 'Range';
+                document.getElementById('verbal').innerText = 'Verbal';
+                document.getElementById('somatic').innerText = 'Somatic';
+                document.getElementById('duration').innerText = 'Duration';
+                document.getElementById('materials').innerText = 'Materials';
+                document.getElementById('ritual').innerText = 'Ritual';
+
+                th.innerText = columnText + ' ▼';
                 this.setState((state) => {
                     return {
-                        spells: this.mySort(state.spells)
+                        spells: this.mySort(state.spells, sortByColumn, columnType, 'descending'),
+                        previousSortState: "descending",
+                        previousSort: sortByColumn
                     };
                 });
-                console.log(this.state.spells);
             }
         });
     }
 
-    mySort(list) {
-        return list.sort(function (a, b) {
-            const x = a.name.toLowerCase();
-            const y = b.name.toLowerCase();
-            if (x < y) { return -1; }
-            if (x > y) { return 1; }
-            return 0;
-        })
+    mySort(list, columnName, columnType, direction) {
+        if (direction === 'descending') {
+            if (columnType === 'string') {
+                return list.sort(function (a, b) {
+                    const x = a[columnName].toLowerCase();
+                    const y = b[columnName].toLowerCase();
+                    if (x < y) { return -1; }
+                    if (x > y) { return 1; }
+                    return 0;
+                });
+            }
+            else {
+                return list.sort(function (a, b) {
+                    return a[columnName] - b[columnName];
+                });
+            }
+        }
+        else if (direction === 'ascending') {
+            if (columnType === 'string') {
+                return list.sort(function (a, b) {
+                    const x = a.name.toLowerCase();
+                    const y = b.name.toLowerCase();
+                    if (x > y) { return -1; }
+                    if (x < y) { return 1; }
+                    return 0;
+                });
+            } 
+            else {
+                return list.sort(function (a, b) {
+                    return b[columnName] - a[columnName];
+                });
+            }
+        }
+        else {
+            return list.sort(function (a, b) {
+                return a.id - b.id;
+            });
+        }
     }
 
     render() {
@@ -92,7 +169,7 @@ class Index extends Component {
             );
         }
         return (
-            <div className="mt-2">
+            <div className="full-page">
                 <h1>Spell Index Page</h1>
                 <div>
                     <Link to="/Spell/Create" className="btn btn-primary btn-lg mb-2">Create Spell</Link>
@@ -100,17 +177,16 @@ class Index extends Component {
                     <table id="spellTable" className="table table-sm table-hover">
                         <thead id="thead" className="thead thead-dark">
                             <tr>
-                                <th data="nameCol">Name</th>
-                                <th>Level</th>
-                                <th>School</th>
-                                <th>Cast Time</th>
-                                <th>Range</th>
-                                <th>Verbal</th>
-                                <th>Somatic</th>
-                                <th>Materials</th>
-                                <th>Duration</th>
-                                <th>Ritual</th>
-                                <th>Description</th>
+                                <th id="name" data-columnname="name" data-columntype="string">Name</th>
+                                <th id="level" data-columnname="level" data-columntype="int">Level</th>
+                                <th id="school" data-columnname="school" data-columntype="string">School</th>
+                                <th id="castingTime" data-columnname="castingTime" data-columntype="string">Cast Time</th>
+                                <th id="range" data-columnname="range" data-columntype="string">Range</th>
+                                <th id="verbal" data-columnname="verbal" data-columntype="bool">Verbal</th>
+                                <th id="somatic" data-columnname="somatic" data-columntype="bool">Somatic</th>
+                                <th id="materials" data-columnname="materials" data-columntype="string">Materials</th>
+                                <th id="duration" data-columnname="duration" data-columntype="string">Duration</th>
+                                <th id="ritual" data-columnname="ritual" data-columntype="bool">Ritual</th>
                             </tr>
                         </thead>
                         <tbody id="tbody">
