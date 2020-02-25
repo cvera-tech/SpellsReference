d0b56177d57b0461856c74d3d9d94d4ad9f3d748
jbvera <johnbenedictv@gmail.com>
2020-02-25 13:21:58 -0500

    Add Index view stubs.
    - Added shared layout.
    - Configured application to use MVC routing.

--- src-core/SpellsReferenceCore/Startup.cs
+++ src-core/SpellsReferenceCore/Startup.cs
@@ -16,6 +16,8 @@ namespace SpellsReferenceCore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +32,8 @@ namespace SpellsReferenceCore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("Default", "{controller=Default}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
